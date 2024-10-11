bf253067410aadb27f310ea7e635ac673f8dcc8a
Elizeu Vito Santos <elizeu.vitor.monteiro@gmail.com>
2024-10-11 16:05:11 -0300

    first commit

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -26,6 +26,12 @@ export class TasksComponent implements OnInit {
     });
   }
 
+  AddTask(tarefa: Tarefa) {
+    this.taskService.addTask(tarefa).subscribe((tarefa) => {
+      this.tarefas.push(tarefa);
+    });
+  }
+
   deleteTask(tarefa: Tarefa) {
 
     this.taskService.deleteTask(tarefa).subscribe(() =>
